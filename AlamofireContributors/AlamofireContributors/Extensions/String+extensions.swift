e65daf660d34cf44af8a4d5406f890c6d266489b
Max Tymchii <samback@ukr.net>
2018-07-04 14:32:15 +0300

    Add string extension.

---
+++ AlamofireContributors/AlamofireContributors/Extensions/String+extensions.swift
@@ -0,0 +1,15 @@
+//
+//  String+extensions.swift
+//  AlamofireContributors
+//
+//  Created by Max Tymchii on 7/4/18.
+//  Copyright © 2018 Max Tymchii. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static var textPlaceHolder: String {
+        return "Empty info"
+    }
+}
